f9d8c3b2721aca64bd6628a3e78601c0a6921286
riforik <isaiah.robinson1029@gmail.com>
2019-02-16 12:36:34 -0500

    Further Front-Page Development
    - Adding functionality and content

--- front-page.php
+++ front-page.php
@@ -12,15 +12,28 @@
  * @package durhamtaxhelp
  */
 
-
+get_header();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div id='splash' class="landing-page">
-				<?php get_header(); ?>
+				<!-- Main Content for Front-Page -->
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-				<h1>Title</h1>
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+				?>
+
+				<!-- After initial content... -->
 				<?php
 					if ( function_exists( 'get_field' ) ) {
 					$home_contents = get_field( 'home_contents' );
@@ -47,6 +60,35 @@
 			}
 			?>
 			</div>
+		</div>
+		<!-- Locations Page -->
+		<div id='locations' class="locations-page">
+			<h4 id="locations_page">Locations</h4>
+			<div id="map-section">
+			<div id="map-list">
+				<div class="map-list-option" data-id="0" data-name='divFinGrp'>
+					<h3>Diverse Financial Group</h3>
+				 <h4>190 Harwood Avenue South (inside G Centre), Ajax</h4>
+				</div>
+				<div class="map-list-option" data-id="1" data-name='durhamComLegClin'>
+					<h3>Durham Community Legal Clinic</h3>
+				 <h4>200 John Street West, Unit B1, Oshawa</h4>
+				</div>
+			</div>
+
+			<div id="map-box">
+			 <div id="leafletMap"></div>
+			</div>
+
+			<div id="map-info">
+			 <h1>Diverse Financial Group</h1>
+			 <h2>190 Harwood Avenue South (inside G Centre), Ajax</h2>
+			 <p>By apointment only</p>
+			 <p>Current and prior year returns</p>
+			 <p>call 647-887-8725</p>
+			</div>
+			</div>
+		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
