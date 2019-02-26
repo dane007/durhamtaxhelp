5ff379bcb1081751e7d260694ddee129f23799e6
riforik <isaiah.robinson1029@gmail.com>
2019-02-26 18:29:38 -0500

    Locations Added
    - locations page added and formatted in terms of style and structure

--- front-page.php
+++ front-page.php
@@ -18,6 +18,7 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div id='splash' class="landing-page">
+        <div class="page1">
 				<div class="site-branding">
 					<?php
 					// the_custom_logo();
@@ -40,14 +41,14 @@
 					$statement = $home_content['statement'];
 					$image = $home_content['image'];
 					$conversion_messages = $home_content['conversion_messages'];
-
+					$findhelp = $home_content['find_help_button'];
+					$qualify = $home_content['convert_button'];
 
           // Conversion statements
           $begin = $conversion_messages['begin'];
-          $failed = $conversion_messages['failed'];
-          $failed = $conversion_messages['success'];
-          $invalid = $conversion_messages['invalid'];
-
+          // $failed = $conversion_messages['failed'];
+          // $failed = $conversion_messages['success'];
+          // $invalid = $conversion_messages['invalid'];
 
 					// var_dump( $image );
 				?>
@@ -66,11 +67,11 @@
 						<input class="userInp" type="number" name="" value="" placeholder="30000">
 					</div>
 					<div class="btn small-6 medium-2 large-3">
-						<button class="calculate" type="button" name="button">QUALIFY</button>
+						<button class="calculate" type="button" name="button"><?php echo $qualify; ?></button>
 					</div>
 					<div class="bigbtn small-12">
 						<a href="https://www.isaiahrobinson.ca">
-						<button class="findTxHp" type="button" name="button">Find Tax Help</button>
+						<button class="findTxHp" type="button" name="button"><?php echo $findhelp; ?></button>
 						</a>
 					</div>
 				<?php
@@ -81,7 +82,35 @@
 			?>
 			</div>
 		</div>
+    <!-- Locations Page -->
+		<div id='locations' class="locations-page page2">
+			<div id="map-section" class="grid-x">
+				<h4 class="title small-12">Locations</h4>
+				<div id="map-list" class="small-12 medium-12 large-3">
+					<div class="map-list-option" data-id="0" data-name='divFinGrp'>
+						<h3>Diverse Financial Group</h3>
+						<h4>190 Harwood Avenue South (inside G Centre), Ajax</h4>
+					</div>
+						<div class="map-list-option" data-id="1" data-name='durhamComLegClin'>
+							<h3>Durham Community Legal Clinic</h3>
+						 <h4>200 John Street West, Unit B1, Oshawa</h4>
+						</div>
+				</div>
 
+				<div id="map-box" class="small-12 medium-12 large-6">
+				 	<div id="leafletMap"></div>
+				</div>
+
+				<div id="map-info" class="small-12 medium-12 large-3">
+					 <h3>Diverse Financial Group</h3>
+					 <h4>190 Harwood Avenue South (inside G Centre), Ajax</h4>
+					 <p>By apointment only</p>
+					 <p>Current and prior year returns</p>
+					 <p>call 647-887-8725</p>
+				</div>
+  		 </div>
+      </div>
+    </div>
 		</main>
 	</div><!-- #primary -->
 
